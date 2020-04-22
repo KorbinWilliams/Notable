8b137f6af64518eff00dbeb39bd9e7b63ce69478
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2020-04-21 19:22:15 -0600

    active month shows, days adjusted based on month

--- client/src/views/Calendar.vue
+++ client/src/views/Calendar.vue
@@ -2,8 +2,14 @@
   <div class="calendar container-fluid">
     <div class="row top-bar">
       <!-- Display active month if there is no active month set active month to current month -->
-      <div class="col-12">
-        <h1>{{Month}}</h1>
+      <div class="col-3">
+        <img src alt="previous month" />
+      </div>
+      <div class="col-6">
+        <h1>{{Month.name.default}}</h1>
+      </div>
+      <div class="col-3">
+        <img src alt="next month" />
       </div>
     </div>
     <div class="row">
@@ -21,46 +27,131 @@
         -->
         <!-- NOTE ARRANGING DAYS IN CALENDER: (Can I make a more customized v-for? if so, v-for over month.days let month.days[0].dow = curDow )  -->
         <div class="row d-flex justify-content-center">
-          <div class="col-1 cal-block">1</div>
-          <div class="col-1 cal-block">2</div>
-          <div class="col-1 cal-block">3</div>
-          <div class="col-1 cal-block">4</div>
-          <div class="col-1 cal-block">5</div>
-          <div class="col-1 cal-block">6</div>
-          <div class="col-1 cal-block">7</div>
+          <div class="col-1 cal-dow">
+            <p>Sun</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Mon</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Tues</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Wed</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Thurs</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Fri</p>
+          </div>
+          <div class="col-1 cal-dow">
+            <p>Sat</p>
+          </div>
         </div>
         <div class="row d-flex justify-content-center">
-          <div class="col-1 cal-block">8</div>
-          <div class="col-1 cal-block">9</div>
-          <div class="col-1 cal-block">10</div>
-          <div class="col-1 cal-block">11</div>
-          <div class="col-1 cal-block">12</div>
-          <div class="col-1 cal-block">13</div>
-          <div class="col-1 cal-block">14</div>
+          <div class="col-1 cal-block">
+            <p>1</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>2</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>3</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>4</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>5</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>6</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>7</p>
+          </div>
         </div>
         <div class="row d-flex justify-content-center">
-          <div class="col-1 cal-block">15</div>
-          <div class="col-1 cal-block">16</div>
-          <div class="col-1 cal-block">17</div>
-          <div class="col-1 cal-block">18</div>
-          <div class="col-1 cal-block">19</div>
-          <div class="col-1 cal-block">20</div>
-          <div class="col-1 cal-block">21</div>
+          <div class="col-1 cal-block">
+            <p>8</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>9</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>10</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>11</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>12</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>13</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>14</p>
+          </div>
         </div>
         <div class="row d-flex justify-content-center">
-          <div class="col-1 cal-block">22</div>
-          <div class="col-1 cal-block">23</div>
-          <div class="col-1 cal-block">24</div>
-          <div class="col-1 cal-block">25</div>
-          <div class="col-1 cal-block">26</div>
-          <div class="col-1 cal-block">27</div>
-          <div class="col-1 cal-block">28</div>
+          <div class="col-1 cal-block">
+            <p>15</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>16</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>17</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>18</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>19</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>20</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>21</p>
+          </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="col-1 cal-block">
+            <p>22</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>23</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>24</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>25</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>26</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>27</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p>28</p>
+          </div>
         </div>
         <!-- NOTE Start v-ifs here feb has 29 on leap year, 30 or 31 for other months. -->
         <div class="row d-flex justify-content-center">
-          <div class="col-1 cal-block">29</div>
-          <div class="col-1 cal-block">30</div>
-          <div class="col-1 cal-block">31</div>
+          <div class="col-1 cal-block">
+            <p>29</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p v-if="this.$store.state.activeMonth.days.default.length > 29">30</p>
+          </div>
+          <div class="col-1 cal-block">
+            <p v-if="this.$store.state.activeMonth.days.default.length > 30">31</p>
+          </div>
           <div class="col-1 cal-block"></div>
           <div class="col-1 cal-block"></div>
           <div class="col-1 cal-block"></div>
@@ -92,24 +183,21 @@ export default {
         .toJSON()
         .slice(0, 10)
         .replace(/-/g, "/");
-      console.log(currentDateWithFormat + " current date");
 
+      // TODO Fix this up using slice
       let curMonthStr =
         currentDateWithFormat[5].toString() +
         currentDateWithFormat[6].toString();
       let curMonth = Number(curMonthStr);
-      console.log(curMonthStr + " curMonth");
 
       let cal = this.$store.state.calendar[0].Months;
-      let activeMonth = cal.find(
-        m => m.order == curMonth[1] || m.order == curMonth
-      );
+      let activeMonth = cal.find(m => m.order == curMonth);
+      // TODO Fix this so that acctiveMonth contains the whole month object then adjust days based on minth
       this.$store.dispatch("setActive", {
         commit: "setItem",
         commitAddress: "activeMonth",
-        data: activeMonth.name.default
+        data: activeMonth
       });
-      console.log(activeMonth.name.default + " default");
     }
   },
   computed: {
@@ -127,6 +215,10 @@ export default {
 .cal-block {
   height: 10rem;
   color: whitesmoke;
+}
+.cal-dow {
+  height: 2rem;
+  color: lightsteelblue;
 }
 /* TODO Make v-if for background color dependent on month */
 body {
